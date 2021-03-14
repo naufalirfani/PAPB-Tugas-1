0f569d8e3321c65fa61bed5f8fd061e8291dc611
denzellaimena <denzel.laimena@gmail.com>
2021-03-14 20:18:36 +0700

    fin

---
+++ testAndroid/app/src/main/java/com/example/nav/InputFragment.kt
@@ -0,0 +1,40 @@
+package com.example.nav
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+
+class InputFragment : Fragment(), View.OnClickListener {
+
+    lateinit var navController: NavController
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_input, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        view.findViewById<Button>(R.id.next_btn).setOnClickListener(this)
+        view.findViewById<Button>(R.id.cancel_btn).setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            R.id.next_btn -> {navController!!.navigate(R.id.action_inputFragment_to_secondFragment)}
+            R.id.cancel_btn -> requireActivity().onBackPressed()
+        }
+    }
+
+
+}
---
+++ testAndroid/app/src/main/java/com/example/nav/MainFragment.kt
@@ -0,0 +1,37 @@
+package com.example.nav
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import kotlin.math.log
+
+class MainFragment : Fragment(), View.OnClickListener {
+
+    lateinit var navController: NavController
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        view.findViewById<Button>(R.id.nama_btn).setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+    when(v!!.id){ //view is not null
+        R.id.nama_btn -> navController!!.navigate(R.id.action_mainFragment5_to_inputFragment) //navigate ke fragment input
+    }
+    }
+
+}
