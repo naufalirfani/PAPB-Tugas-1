f8ab6be9f90b8e0d3fb4949d80025e1010e0c146
Naufal Irfani <naufal.irfani3@gmail.com>
2021-03-15 09:20:41 +0700

    update viewpager adn tablayout

--- app/src/main/java/com/example/navigation/MainActivity.kt
+++ app/src/main/java/com/example/navigation/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
 
         supportActionBar?.title = "ViewPager and TabLayout"
 
-        binding.mainTab.setTabTextColors(Color.parseColor("#BDBDBD"), Color.parseColor("#000000"))
+        binding.mainTab.setTabTextColors(Color.parseColor("#BDBDBD"), Color.parseColor("#FFFFFF"))
 
         val pagerAdapter = PagerAdapter(supportFragmentManager)
         binding.pager.adapter = pagerAdapter
--- app/src/main/java/com/example/navigation/adapter/PagerAdapter.kt
+++ app/src/main/java/com/example/navigation/adapter/PagerAdapter.kt
@@ -3,6 +3,9 @@ package com.example.navigation.adapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
+import com.example.navigation.ui.beranda.FragmentBeranda
+import com.example.navigation.ui.kategori.FragmentKategori
+import com.example.navigation.ui.status.FragmentStatus
 
 @Suppress("DEPRECATION")
 class PagerAdapter (fm: FragmentManager) : FragmentStatePagerAdapter(fm){
